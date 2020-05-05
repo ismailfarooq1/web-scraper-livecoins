a9a7d6450d2e6e6c57ce75373651e3cc772b1c69
Ismail Farooq <ismaeelfarooq53@gmail.com>
2020-05-06 04:36:41 +0500

    Using SCrapy with Splash using Dockers

---
+++ livecoin/livecoin/spiders/coin.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class CoinSpider(scrapy.Spider):
+    name = 'coin'
+    allowed_domains = ['www.livecoin.net/en']
+    
+
+    script = '''
+        function main(splash, args)
+        splash.private_mode_enabled = false
+        url = args.url
+        assert(splash:go(url))
+        assert(splash:wait(1))
+        
+        usd_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
+        usd_tab[3]:mouse_click()
+        assert(splash:wait(1))
+        splash:set_viewport_full()
+        return {
+            splash:html()
+        }
+        end
+    '''
+
+    def start_requests(self):
+        yield SplashRequest(url="https://www.livecoin.net/en/", callback=self.parse, endpoint="execute", args={
+            'lua_source': self.script
+        })
+
+         
+
+    def parse(self, response):
+
+        for row in response.xpath("//div[contains(@class, 'ReactVirtualized__Table__row tableRow___3EtiS')]"):
+            print("\nHELLO\n")
+            # name = row.xpath(".//div[@class='ReactVirtualized__Table__rowColumn tableRowColumn___rDsl0']/div/text()").get()
+
+            yield {
+                'name': row.xpath(".//div[@class='ReactVirtualized__Table__rowColumn tableRowColumn___rDsl0']/div/text()").get(),
+                # 'volume(24h)': row.xpath(".//").get()
+            }
+
